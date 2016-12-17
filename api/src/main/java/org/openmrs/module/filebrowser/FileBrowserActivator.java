fd8b78c26acd735fd23ef1ba6ad351c6ab94f1d4
dkayiwa <kayiwadaniel@gmail.com>
2016-12-17 22:37:01 +0300

    Making the module run on platform 2.x

--- api/src/main/java/org/openmrs/module/filebrowser/FileBrowserActivator.java
+++ api/src/main/java/org/openmrs/module/filebrowser/FileBrowserActivator.java
@@ -14,14 +14,16 @@
 package org.openmrs.module.filebrowser;
 
 
-import org.apache.commons.logging.Log; 
+import java.rmi.activation.Activator;
+
+import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.openmrs.module.Activator;
+import org.openmrs.module.BaseModuleActivator;
 
 /**
  * This class contains the logic that is run every time this module is either started or stopped.
  */
-public class FileBrowserActivator implements Activator {
+public class FileBrowserActivator extends BaseModuleActivator {
 	
 	protected Log log = LogFactory.getLog(getClass());
 		
